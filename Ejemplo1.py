1a31661b2c83ea3be7881ebeec225a2912943df2
309 <309@academicos.cesde.edu.co>
2023-08-15 08:37:25 -0500

    feat:se reciben datos por teclado y se castea

--- Ejemplo1.py
+++ Ejemplo1.py
@@ -1,13 +1,24 @@
 # numeral para comentario de linea
-nombreUsuario = "Juan murillo"
+'''nombreUsuario = "Juan murillo"
 edadUsuario = 23
 estaturaUsuario = 1.75
 esHinchaDelVerde = True
-'''tres comillas simples 
-para comentar por bloque'''
+''tres comillas simples 
+para comentar por bloque'' 
 
 print(f"Su nombre es: {nombreUsuario} y tiene {edadUsuario} años.")
 
 comidasFavoritas = ["frijoles","sudado","sancocho","chicharron","chocolate"]
 print(comidasFavoritas)
 print(comidasFavoritas[2])
+
+
+#entradas por teclado
+lugarTrabajoUsuario=input("Digita el lugar de trabajo: ")
+print(f"Usted trabaja en: {lugarTrabajoUsuario}")'''
+
+#pedimos dos numeros
+numeroUno = int(input("Digita un numero: "))
+numeroDos = int(input("Digita un numero: "))
+sumatoria = numeroUno + numeroDos
+print(f"La suma de {numeroUno}+{numeroDos}={sumatoria}")
